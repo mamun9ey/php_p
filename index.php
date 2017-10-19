0d0af731e3a8f09aa3906ba3b5a36d0ebff23652
Abdullah <abdullahmamun8020@gmail.com>
2017-10-20 01:27:25 +0600

    php pratice

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>my frist php page</h1>
+    <?php
+    echo "my frist php script";
+    ?>
+    <?php 
+    $txt ="helo world";
+    $x = 5;
+    $y = 10.5;
+    ?>
+    <?php 
+$names = ['tuhin', 'sipon', 'mamun'];   
+$names[0];     
+$names = [
+$names = ['tuhin', 'sipon', 'mamun'];   
+  'name1' => 'tuhin',
+  'name2' => 'Sipon',
+  'name3' => 'mamun'
+];
+$names['name2'] = 'sarwar'
+?>
+  //poly
+
+  interface Parent {
+
+}
+interface Uncle {
+
+}
+class Child implements Parent, Uncle {
+
+}
+//function
+$names = ['tuhin', 'sipon', 'mamun'];   
+
+</body>
+</html>
